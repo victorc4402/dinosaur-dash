5179638e24d4a6a480f0dca6cce63d4c26f67ed0
oweny314 <261070021+oweny314@users.noreply.github.com>
2026-09-25 16:45:13 -0400

    Added level and obstacles

---
+++ Assets/obstacle.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+
+public class obstacle : MonoBehaviour
+{
+    public float obstaclespeed = 0.00005f;
+    public float startpos = 1;
+    int moving;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Vector2 currentPosition = transform.position;
+        transform.position = new Vector2(startpos,-2.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(-obstaclespeed*Time.deltaTime,0);
+        if (transform.position[0] < -14)
+        {
+            transform.position = new Vector2(13,-2.5f);
+        }
+    }
+}
